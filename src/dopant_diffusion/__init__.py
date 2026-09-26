3fe871f5974e0867790df1cc9ab333bca60ffb76
Atabrahim <morad.seller21@gmail.com>
2026-09-26 18:48:04 +0200

    Expose validated process workflow in public API

--- src/dopant_diffusion/__init__.py
+++ src/dopant_diffusion/__init__.py
@@ -1,6 +1,18 @@
 """Conservative one-dimensional silicon dopant-diffusion simulation."""
 
 from .materials import BoronInSilicon
+from .solver import BoundaryCondition, SpatialGrid, solve_diffusion
 from .thermal import ThermalSchedule, ThermalSegment
+from .workflow import FiniteSourceCase, SimulationOutcome, run_finite_source_case
 
-__all__ = ["BoronInSilicon", "ThermalSchedule", "ThermalSegment"]
+__all__ = [
+    "BoronInSilicon",
+    "BoundaryCondition",
+    "FiniteSourceCase",
+    "SimulationOutcome",
+    "SpatialGrid",
+    "ThermalSchedule",
+    "ThermalSegment",
+    "run_finite_source_case",
+    "solve_diffusion",
+]
